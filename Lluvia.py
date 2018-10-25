72a23fff584d754486b63aa3c94000b860003dce
keilagonzalez <44431533+keilagonzalez@users.noreply.github.com>
2018-10-25 11:56:00 +0200

    Create Lluvia.py

---
+++ Lluvia.py
@@ -0,0 +1,6 @@
+#Necesitas paraguas?
+humedad = int(input("Cual es el grado de humedad de hoy? "))
+if (humedad >= 80):
+  print("Cuidado, con", humedad, "% de humedad puede llover, necesitas paraguas!!")
+else:
+  print ("Tranquilidad, hoy hace buen tiempo, deja el paraguas en casa.")
