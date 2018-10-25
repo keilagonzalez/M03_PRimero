cec0d934df5f1c2c5bf8bf25c216fc4e2dda177c
keilagonzalez <44431533+keilagonzalez@users.noreply.github.com>
2018-10-25 11:43:59 +0200

    Update Aprovar M03.py

--- Aprovar M03.py
+++ Aprovar M03.py
@@ -1,7 +1,6 @@
-#Quieres aprovar M03
 jamon = input("Has comprado jamon? ")
 estudiar = input ("Has estudiado? ")
 abduccion = input ("Te han abducido? ")
 dia = input ("Cuando te han abducido? ")
-if (jamon == "si") or (estudiar== "mucho") or ((abduccion=="si")and(dia="jueves")):
-  print("estás aprovado!!)
+if (jamon == "si") or (estudiar== "mucho") or ((abduccion=="si")and(dia=="jueves")):
+    print("Estas aprovado!!")
