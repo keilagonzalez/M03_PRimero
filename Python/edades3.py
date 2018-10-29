b9dbd6d12f76cde5c75fcd624e3a998d937b70a3
keilagonzalez <44431533+keilagonzalez@users.noreply.github.com>
2018-10-29 19:40:55 +0100

    Create edades3.py

---
+++ Python/edades3.py
@@ -0,0 +1,5 @@
+edad=int(input("Cuantos años tienes? "))
+if ((edad>=18) and (edad<=23)) or (edad==17) :
+    print ("Con", edad, "años, puedes entrar a la sesión de jovenes")
+else: 
+    print ("Con", edad, "años, esto no es para ti")
