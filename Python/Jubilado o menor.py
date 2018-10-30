e962fae70896150b4c3c6093e2aafc173c9e812e
keilagonzalez <44431533+keilagonzalez@users.noreply.github.com>
2018-10-30 20:02:13 +0100

    Update and rename Jubilado o menor.py to Preu_a_pagar_2.py

--- Python/Jubilado o menor.py
+++
@@ -1,5 +0,0 @@
-edad = int(input("Cuantos años tienes? "))
-if (edad >=65) or (edad<5) :
-    print ("Con", edad, "años, te sale gratis")
-else :
-    print ("Con", edad, "años, pagas 2.5€")
---
+++ Python/Preu_a_pagar_2.py
@@ -0,0 +1,5 @@
+edad = int(input("Cuantos anyos tienes? "))
+if (edad >=65) or (edad<5) :
+    print ("Con", edad, "anyos, te sale gratis")
+else :
+    print ("Con", edad, "anyos, pagas 2.5€")
