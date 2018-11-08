beee890a7723b65301682ce3ade3b4fb4e8a3268
keilagonzalez <44431533+keilagonzalez@users.noreply.github.com>
2018-11-08 12:47:30 +0100

    Create ordena_numeros.py

---
+++ Python/ordena_numeros.py
@@ -0,0 +1,27 @@
+# Entrada | Sortida #
+#   1,2,3 | 1,2,3   #
+#   1,3,2 | 1,2,3   #
+#   2,1,3 | 1,2,3   # 
+#   2,3,1 | 1,2,3   #
+#   3,2,1 | 1,2,3   #
+#   3,1,2 | 1,2,3   #
+#   1,1,1 | 1,1,1   #
+#   1,1,2 | 1,1,2   #
+#   2,1,1 | 1,1,3   #
+#   2,1,2 | 1,2,2   #
+primer=int(input("Introdueix el primer numero: "))
+segon=int(input("Introdueix el segon numero: "))
+tercer=int(input("Introdueix el tercer numero: "))
+if(primer<segon<tercer):
+    print(primer,",",segon,",",tercer) 
+else:
+    if(primer<segon>tercer):
+        print(primer,",",tercer,",", segon)
+    else:
+        if(primer>segon<tercer):
+            print(segon,",",primer,",",tercer)
+        else: 
+           
+#if(primer==segon==tercer):#
+    #print(primer,",",segon,",",tercer)#
+#else:#
