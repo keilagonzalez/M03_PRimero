9196cd1da0a88b8a472c164d579aa2c491b442eb
keilagonzalez <44431533+keilagonzalez@users.noreply.github.com>
2018-11-09 22:18:56 +0100

    Update and rename ordena_numeros.py to Ordena_3_numeros.py

--- Python/Ordena_3_numeros.py
+++ Python/Ordena_3_numeros.py
@@ -56,5 +56,43 @@ else:
                                                 else:
                                                     if(primer==tercer<segon>primer):
                                                         print(primer,",",tercer,",",segon)
-                                                    
-                                            
+                                                        
+                                                        
+                                      
+                                     ¡¡¡MILLORA!!! 
+                            
+                            
+# Entrada | Sortida #
+#   1,2,3 | 1,2,3   #
+#   1,3,2 | 1,2,3   #
+#   2,1,3 | 1,2,3   # 
+#   2,3,1 | 1,2,3   #
+#   3,2,1 | 1,2,3   #
+#   3,1,2 | 1,2,3   #
+#   1,1,1 | 1,1,1   #
+#   1,1,2 | 1,1,2   #
+#   2,2,1 | 1,2,2   #
+#   1,2,2 | 1,2,2   #
+#   2,1,1 | 1,1,2   #
+#   2,1,2 | 1,2,2   #
+#   2,3,2 | 2,2,3   #
+primer=int(input("Introdueix el primer numero: "))
+segon=int(input("Introdueix el segon numero: "))
+tercer=int(input("Introdueix el tercer numero: "))
+if(primer<segon<tercer>primer)or(primer==segon==tercer==primer)or(primer==segon<tercer>primer)or(primer<segon==tercer>primer):
+    print(primer,",",segon,",",tercer) 
+else:
+    if(primer<segon>tercer>primer)or(primer==tercer<segon>primer):
+        print(primer,",",tercer,",", segon)
+    else:
+        if(primer>segon<tercer>primer)or(primer==tercer>segon<primer):
+            print(segon,",",primer,",",tercer)
+        else:
+            if(primer<segon>tercer<primer)or(primer==segon>tercer<primer):
+                print(tercer,",",primer,",",segon)
+            else:
+                if(primer>segon>tercer<primer)or(primer==segon>tercer):
+                    print(tercer,",",segon,",",primer)
+                else:
+                    if(primer>segon<tercer<primer)or(primer>segon==tercer<primer):
+                        print(segon,",",tercer,",",primer)
