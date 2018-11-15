c436e5544e9ae0229620f1d5cc52ddaebab79cc9
keilagonzalez <44431533+keilagonzalez@users.noreply.github.com>
2018-11-15 10:22:53 +0100

    Create Maxims.py

---
+++ Python/Maxims.py
@@ -0,0 +1,15 @@
+#MAXIMS AMB 2 NUMEROS#
+
+"""
+1,1|Primer més gran
+2,1|Primer més gran
+1,2|segon més gran
+"""
+primer=int(input("Introdueix el primer numero "))
+segon=int(input("Introdueix el segon numero "))
+if (primer>=segon):
+        print("El mes gran es el", primer,)
+else:
+        print("El mes gran es el", segon)
+       
+       
