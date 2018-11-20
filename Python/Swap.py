531c04fe3737177a9083324a474bebb52576bc2b
keilagonzalez <44431533+keilagonzalez@users.noreply.github.com>
2018-11-20 09:38:47 +0100

    Create Swap.py

---
+++ Python/Swap.py
@@ -0,0 +1,7 @@
+Derecha="Movil"
+Izquierda="Bocadillo"
+Tercera_Mano=Derecha
+Derecha=Izquierda
+Izquierda=Tercera_Mano
+print("En la mano derecha tienes el", Derecha)
+print("En la mano izquierda tienes el", Izquierda)
