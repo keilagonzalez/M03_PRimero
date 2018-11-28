197368e60ddf60fe76c195dafa3815069f9853a5
keilagonzalez <44431533+keilagonzalez@users.noreply.github.com>
2018-11-28 09:41:15 +0100

    Create condicion_chunga.py

---
+++ Python/condicion_chunga.py
@@ -0,0 +1,13 @@
+numero=int(input("Introduzca aqui su numero: "))
+if(numero%2==0):
+    print("Es un numero par.")
+else:
+    print("Es un numero impar.")
+if(numero<=40)and(numero>=-10):
+    print("Esta entre 40 y -10.")
+else:
+    print("No esta entre 40 y -10.")
+if(numero<0):
+    print("Es negativo.")
+else:
+    print("No es negativo.")
