efa443c04cd24a5730c668747b62dd455d0da4a1
keilagonzalez <44431533+keilagonzalez@users.noreply.github.com>
2018-11-30 00:10:08 +0100

    Rename Swap4.py to Swap_4.py

---
+++ Python/Swap_4.py
@@ -0,0 +1,14 @@
+cajon1="movil"
+cajon2="bocadillo"
+cajon3="boli"
+cajon4="bebida"
+extra=cajon3
+cajon3=cajon1
+cajon1=extra
+extra=cajon2
+cajon2=cajon4
+cajon4=extra
+print("Cajon 1:", cajon1,)
+print("Cajon 2:", cajon2,)
+print("Cajon 3:", cajon3,)
+print("Cajon 4:", cajon4,)
