9b226c753638da0646058978e9f75523f715ca55
keilagonzalez <44431533+keilagonzalez@users.noreply.github.com>
2018-12-02 22:30:07 +0100

    Update Pi_pa_ti.py

--- Python/Pi_pa_ti.py
+++ Python/Pi_pa_ti.py
@@ -1,3 +1,19 @@
+print("MENU:")
+print("  PIEDRA")
+print("  PAPEL")  
+print("  TIJERAS")
+usuario=input("Introduzca su seleccion: ")
+usuario.lower()
 from random import choice
-posibilidades=("Piedra","Papel","Tijeras")
-print(choice(posibilidades))
+posibilidades=["piedra","papel","tijeras"]
+maquina=(choice(posibilidades))
+print("Tu has sacado: ",usuario,)
+print("La maquina ha sacado: ",maquina,)
+if(usuario==maquina):
+    print("Empate")
+else:
+    if((usuario=="piedra")and(maquina=="tijeras"))or((usuario=="papel")and(maquina=="piedra"))or((usuario=="tijeras")and(maquina=="papel")):
+        print("Has ganado!!")
+    else:
+        if((maquina=="piedra")and(usuario=="tijeras"))or((maquina=="papel")and(usuario=="piedra"))or((maquina=="tijeras")and(usuario=="papel")):
+            print("Ha ganado la maquina...")
